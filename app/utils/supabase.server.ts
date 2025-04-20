fe46c9857d3f99b25b9210fe99ec7109a4645b28
Henry Robbins <hw.robbins@gmail.com>
2025-04-20 18:01:55 -0400

    Use SUPABASE_SERVICE_ROLE_KEY instead of ANON_KEY to avoid auth for now

--- app/utils/supabase.server.ts
+++ app/utils/supabase.server.ts
@@ -6,6 +6,6 @@ export const getSupabaseServerClient = (
 ) =>
   createServerClient(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     { request, response: new Response(null, { headers: responseHeaders }) }
   );
