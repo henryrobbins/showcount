4d1547451f1e4746c3906ee0b49f257af8e9e2ad
Henry Robbins <hw.robbins@gmail.com>
2025-04-21 23:54:43 -0400

    Improve text scaling on index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,16 +12,18 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex text-center flex-col items-center gap-5">
-        <h1 className="font-zen leading-tight text-8xl antialiased">
+        <h1 className="font-zen leading-tight antialiased text-center text-[clamp(2rem,10vw,6rem)]">
           showcount.com
         </h1>
-        <p className="font-mono text-1xl font-light">
-          Concert tracking. Coming soon.
+        <p className="font-mono text-[clamp(0.875rem, 2.5vw, 1.25rem)]">
+          Concert tracking.
           <br />
+          Coming soon.
+        </p>
+        <p className="font-mono text-[clamp(0.875rem, 2.5vw, 1.25rem)]">
           <Link to="/list/love-music-will-travel" className="underline hover">
             @love-music-will-travel
           </Link>
-          <br />
         </p>
       </div>
     </div>
