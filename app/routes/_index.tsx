44a5f9bd56cf3b389278e7cf478b26be6d7789a0
Henry Robbins <hw.robbins@gmail.com>
2025-05-18 20:44:28 -0400

    Change title font Instant Zen -> Bocklin

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,8 +12,8 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex text-center flex-col items-center gap-5">
-        <h1 className="font-zen leading-tight antialiased text-center text-[clamp(2rem,10vw,6rem)]">
-          showcount.com
+        <h1 className="font-title leading-tight tracking-tighter antialiased text-center text-[clamp(3.5rem,10vw,5.5rem)]">
+          SHOWCOUNT
         </h1>
         <p className="font-mono text-[clamp(0.875rem, 2.5vw, 1.25rem)]">
           Concert tracking.
